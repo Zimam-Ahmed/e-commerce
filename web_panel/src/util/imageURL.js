71e86be8680dd680f2e3054ae80bb3ab54db6b34
Zimam-Ahmed <128451501+Zimam-Ahmed@users.noreply.github.com>
2024-09-14 16:31:42 +0500

    Update imageURL.js

--- web_panel/src/util/imageURL.js
+++ web_panel/src/util/imageURL.js
@@ -1,6 +1,6 @@
 // imageUtils.js
 const getProductImageUrl = (imagePath) => {
-    return `https://e-commerce-rncbr7p5x-zimam-ahmeds-projects-f14a45e2.vercel.app/public${imagePath}`;
+    return `https://e-commerce-ten-mauve-45.vercel.app/public${imagePath}`;
   };
   
   export default getProductImageUrl;
