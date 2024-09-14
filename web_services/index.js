129df1c187080e5b6275237127ea53fe31bfe4cf
Zimam-Ahmed <128451501+Zimam-Ahmed@users.noreply.github.com>
2024-09-14 14:06:14 +0500

    Update index.js

--- web_services/index.js
+++ web_services/index.js
@@ -26,7 +26,7 @@ mongoose.connect(process.env.DB_URL_DEVELOPMENT)
 //Allow to call from different source
 app.use(cors({
     origin: (origin, callback) => {
-        const allowedOrigins = ['http://localhost:5001', 'http://localhost:5003', 'https://e-commerce-bn064t2jf-zimam-ahmeds-projects-f14a45e2.vercel.app/'];
+        const allowedOrigins = [];
         if (!origin || allowedOrigins.includes(origin)) {
             callback(null, true);
         } else {
